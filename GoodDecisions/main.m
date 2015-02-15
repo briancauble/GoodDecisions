469a4de52dd086a443457af6160a75a78d004e21
Brian Cauble <caublb@charter.net>
2015-02-15 12:12:14 -0600

    Initial Commit

---
+++ GoodDecisions/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoodDecisions
+//
+//  Created by Brian Cauble on 2/15/15.
+//  Copyright (c) 2015 Brian Cauble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
